7c5a54e3b7f11ecb816d4fffd33e7e1b3e41abaa
dot <macairm1@dot.co.id>
2024-10-24 11:10:52 +0700

    fixing: issue deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,10 +71,12 @@ export default function Home() {
       <footer className="bg-white dark:bg-gray-900 mt-64">
         <div className="container flex flex-col items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
           <a href="#">
-            <img
+            <Image
               className="w-auto h-7"
               src="https://merakiui.com/images/full-logo.svg"
               alt=""
+              width={150}
+              height={30}
             />
           </a>
 
